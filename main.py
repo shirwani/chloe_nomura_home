3bc73e93d3e00c0cdab03524d51b94cecb68c700
Zaki Shirwani <zakishirwani@alumni.purdue.edu>
2025-03-06 13:00:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def run():
     return render_template("index.html",
-                           image_path='https://debian-us-east-zakishirwani.com.us-east-1.linodeobjects.com/stuff/puntacana/puntacana.jpg',
+                           image_path='https://debian-us-east-zakishirwani.com.us-east-1.linodeobjects.com/puntacana/puntacana.jpg',
                            js_path='static/js/script.js')
 
 if __name__ == '__main__':
